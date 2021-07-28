d9eb2e67e654cce110e6898c1e27466c095214d2
Greg Cox <gcox@mozilla.com>
2021-07-28 01:00:58 +0000

    Fix pylint issues on the python unit test

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -9,25 +9,30 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
 class YAMLTest(unittest.TestCase):
-    def get_yaml_file(self):
-        apps_yml = open('apps.yml')
-        contents = apps_yml.read()
-        apps_yml.close()
+    ''' Class that tests the apps.yml file for the SSO Dashboard '''
+    @staticmethod
+    def get_yaml_file():
+        ''' fetch the contents of apps.yml as one big blob '''
+        with open('apps.yml') as apps_yml:
+            contents = apps_yml.read()
         return contents
 
     def test_output_exists(self):
+        ''' Verify that apps.yml is present '''
         assert self.get_yaml_file() is not None
 
     def test_yaml_loads(self):
+        ''' Verify that apps.yml is valid yaml '''
         yaml_content = yaml.load(self.get_yaml_file())
         assert yaml_content is not None
 
     def test_each_has_required_keys(self):
+        ''' Verify that apps.yml has keys we expect '''
         yaml_content = yaml.load(self.get_yaml_file())
         assert yaml_content is not None
 
         for app in yaml_content['apps']:
-            logger.info('Validating keys for {}'.format(app['application']))
+            logger.info('Validating keys for %s', app['application'])
             assert app['application']['name'] is not None
             # assert app['application']['client_id'] is not None # Client ID not required
             assert app['application']['op'] is not None
